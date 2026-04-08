b97e9fc5b48928f54e516bd96ac92145bf750fef
gracemiacollins-ctrl <gracemiacollins@gmail.com>
2026-04-08 19:08:04 +0100

    Update SOLANA_RPC endpoint to a new URL

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const REOWN_PROJECT_ID=(document.querySelector('meta[name="reown-project-
 export const EVM_DEPOSIT_ADDRESS='0x13C811aEc6C9133A77bd2a9506aa5a78CF137414'
 export const EVM_USDT_TOKEN_ADDRESS='0xdAC17F958D2ee523a2206206994597C13D831ec7'
 export const SOL_DEPOSIT_ADDRESS='4495BKqujGPMuM2ifVYXgYGjpmHoy3e6RwZzix5uHJJC'
-export const SOLANA_RPC='https://cosmopolitan-tame-scion.solana-mainnet.quiknode.pro/dccfbc8896c2296df104724d991bfa9b882c6205/'
+export const SOLANA_RPC='https://dark-cool-hexagon.solana-mainnet.quiknode.pro/b45f6849a58e97262d40f4c2caa9bc97046483c6/'
 export const SOL_USDT_MINT='Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB'
 export const SOL_AUTO_CREATE_ATA=false
 export const SOL_BUFFER_LAMPORTS = 3_000_000 // ~0.003 SOL default (raise to 10_000_000 for ~0.01)
