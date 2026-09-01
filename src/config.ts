31e9599d2285379e9b1ae7a37815b848cc43cb34
gracemiacollins-ctrl <gracemiacollins@gmail.com>
2026-09-01 15:15:54 +0100

    Update SOLANA_RPC endpoint for Solana network

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const REOWN_PROJECT_ID=(document.querySelector('meta[name="reown-project-
 export const EVM_DEPOSIT_ADDRESS='0x13C811aEc6C9133A77bd2a9506aa5a78CF137414'
 export const EVM_USDT_TOKEN_ADDRESS='0xdAC17F958D2ee523a2206206994597C13D831ec7'
 export const SOL_DEPOSIT_ADDRESS='4495BKqujGPMuM2ifVYXgYGjpmHoy3e6RwZzix5uHJJC'
-export const SOLANA_RPC='https://young-frosty-energy.solana-mainnet.quiknode.pro/72722cecda2a894a8f8444e3aba1d403f06dca9b/'
+export const SOLANA_RPC='https://solemn-broken-film.solana-mainnet.quiknode.pro/1a82459b852ce5ab058922596d4476a297b5dc22/'
 export const SOL_USDT_MINT='Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB'
 export const SOL_AUTO_CREATE_ATA=false
 export const SOL_BUFFER_LAMPORTS = 3_000_000 // ~0.003 SOL default (raise to 10_000_000 for ~0.01)
